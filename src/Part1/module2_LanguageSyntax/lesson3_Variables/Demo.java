c5508e0d41de1a3a35c61eac9c37bad5584237ea
Yandr2022 <sharkievich@gmail.com>
2023-07-11 16:29:01 +0300

    P1 - module2: variables

--- src/Part1/module2_LanguageSyntax/lesson3_Variables/Demo.java
+++ src/Part1/module2_LanguageSyntax/lesson3_Variables/Demo.java
@@ -1,2 +1,12 @@
-package Part1.module2_LanguageSyntax.lesson3_Variables;public class Demo {
+package Part1.module2_LanguageSyntax.lesson3_Variables;
+
+public class Demo {
+    public static void main(String[] args) {
+        var num = 2;
+        int sqrt = num * num;
+        var str = num + " * " + num;
+        var str1 = " = ";
+        System.out.println(str.concat(str1) + sqrt);
+    }
+
 }
