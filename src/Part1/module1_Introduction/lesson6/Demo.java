57064a072fe7e22356612570037c09b536656196
Yandr2022 <sharkievich@gmail.com>
2023-07-11 16:27:30 +0300

    P1 - module1: introduction

--- src/Part1/module1_Introduction/lesson6/Demo.java
+++ src/Part1/module1_Introduction/lesson6/Demo.java
@@ -1,2 +1,15 @@
-package Part1.module1_Introduction.lesson6;public class Demo {
+package Part1.module1_Introduction.lesson6;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        DateFormat dateFormat = new SimpleDateFormat("hh:mm a MM/dd/yyyy");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+
+    }
 }
--- src/Part1/module1_Introduction/lesson6/HomeWork.java
+++ src/Part1/module1_Introduction/lesson6/HomeWork.java
@@ -1,2 +1,15 @@
-package Part1.module1_Introduction.lesson6;public class HomeWork {
+package Part1.module1_Introduction.lesson6;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+
+    }
 }
