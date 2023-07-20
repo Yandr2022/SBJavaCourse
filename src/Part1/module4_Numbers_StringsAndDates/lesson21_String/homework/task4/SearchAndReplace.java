aa112b1751afeefce42f134e1b19b34eb443b913
Yandr <102684712+Yandr2022@users.noreply.github.com>
2023-07-20 11:30:54 +0300

    P1 - module4 Numbers, Strings & Dates: RegEx, added task 4 hw 25 with using RegEx

--- src/Part1/module4_Numbers_StringsAndDates/lesson21_String/homework/task4/SearchAndReplace.java
+++ src/Part1/module4_Numbers_StringsAndDates/lesson21_String/homework/task4/SearchAndReplace.java
@@ -1,14 +1,27 @@
 package Part1.module4_Numbers_StringsAndDates.lesson21_String.homework.task4;
 
+import java.util.regex.Pattern;
+
 public class SearchAndReplace {
     public static void main(String[] args) {
 
-        searchAndReplaceDiamonds("number of credit cards: <1234 5678 9012> 1234, name: Yuli", "****");
+        searchAndReplaceDiamonds("number of credit cards: <1234 5678 9012> 1234 , name: Yuli", "****");
+        searchAndReplaceDiamondsRegEx("number of credit cards: <1234 5678 9012> 1234 , name: Yuli", "****");
+        searchAndReplaceDiamondsRegEx
+                ("number of credit cards: < i dont wont say my card number, maybe 1234> 1234<1234>, name: Yuli"
+                        , "****");
+        searchAndReplaceDiamondsRegEx
+                ("number of credit cards: < не скажу я Вам свой  номер карты, может быть 1234 > 1234 <1234> <>, name: Yuli"
+                        , "****");
     }
 
     static void searchAndReplaceDiamonds(String text, String placeholder) {
         String replace = text.substring(text.indexOf('<') + 1, text.indexOf('>'));
         replace = text.replace(replace, placeholder);
+        System.out.println(replace);
+    }
+    static void searchAndReplaceDiamondsRegEx(String text, String placeholder) {
+        String replace = Pattern.compile("<[^>]*>").matcher(text).replaceAll(placeholder);
         System.out.println(replace);
     }
 }
