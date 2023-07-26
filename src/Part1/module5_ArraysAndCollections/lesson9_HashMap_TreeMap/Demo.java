4c00c8349ac2b64be86e1a8b9b9a6c05777a38fe
Yandr2022 <sharkievich@gmail.com>
2023-07-26 06:38:35 +0300

    P1 - module5 Arrays & Collections: Map Introduction - HashMap And TreeMap

---
+++ src/Part1/module5_ArraysAndCollections/lesson9_HashMap_TreeMap/Demo.java
@@ -0,0 +1,32 @@
+package Part1.module5_ArraysAndCollections.lesson9_HashMap_TreeMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Demo {
+    public static void main(String[] args) {
+
+//        HashMap<String, Integer> goodsList = new HashMap<>(); unsorted
+        TreeMap<String, Integer> goodsList = new TreeMap<>();//sorted by key (alphabetic)
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (String goodName = scanner.nextLine(); !goodName.contains("EXIT"); goodName = scanner.nextLine()) {
+            if (goodName.contains("LIST")) {
+                printMap(goodsList);
+            } else {
+                goodsList.put(goodName, goodsList.containsKey(goodName) ? goodsList.get(goodName) + 1 : 1);
+            }
+        }
+        scanner.close();
+    }
+
+    private static void printMap(Map<String, Integer> map) {
+
+        for (String key : map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+}
